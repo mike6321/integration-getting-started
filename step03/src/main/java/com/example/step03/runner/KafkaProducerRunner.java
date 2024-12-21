b18d8b9a8f2c21d7f3dc9beb2170b300976418bd
mike6321 <mike6321@naver.com>
2024-12-21 23:46:30 +0900

    feature: outbount-topic 에 Producing

---
+++ step03/src/main/java/com/example/step03/runner/KafkaProducerRunner.java
@@ -0,0 +1,40 @@
+package com.example.step03.runner;
+
+import com.example.step03.domain.Domain;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaProducerRunner implements CommandLineRunner {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    private static final String TOPIC = "inbound-topic";
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (int i = 1; i <= 10; i++) {
+            String message = getMessage();
+            kafkaTemplate.send(TOPIC, message);
+        }
+
+        log.info("Published 10 messages to Kafka");
+    }
+
+    private String getMessage() throws JsonProcessingException {
+        Domain domain = Domain.builder()
+                .name("junwoo")
+                .age(34)
+                .build();
+
+        return objectMapper.writeValueAsString(domain);
+    }
+
+}
