517ed43585589697d387ecd802434b7b604c7551
mike6321 <mike6321@naver.com>
2024-12-21 22:21:57 +0900

    feature: integration step01 module init

---
+++ step01/src/test/java/com/example/step01/Step01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.step01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
