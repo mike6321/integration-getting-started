0221ae7174d70587d77fe6375c7c259df75c7c79
mike6321 <mike6321@naver.com>
2024-12-21 23:59:00 +0900

    feature: integration step04 module init

---
+++ step04/src/main/java/com/example/step04/Step04Application.java
@@ -0,0 +1,13 @@
+package com.example.step04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Step04Application.class, args);
+    }
+
+}
