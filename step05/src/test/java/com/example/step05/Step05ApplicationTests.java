6b138668e32e736ea10e02f85a8ef15f005bb4f0
mike6321 <mike6321@naver.com>
2024-12-22 09:50:37 +0900

    feature: integration step05 module init

---
+++ step05/src/test/java/com/example/step05/Step05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.step05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
